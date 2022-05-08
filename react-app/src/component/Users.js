bf757bcf1486401b6d10e728719580fd2c619b79
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-05-08 02:30:21 +0200

    Update Users.js
    some adjustment to make is clearer

--- react-app/src/component/Users.js
+++ react-app/src/component/Users.js
@@ -1,16 +1,14 @@
 import React from "react";
-import UsersData from "../data.json";
+
 
 class Users extends React.Component {
   render() {
     return (
       <>
         <div className="users-container">
-          {UsersData.map((data) => {
-            return (
-              <div key={data.id} className="users">{data.name + " " + data.departments}</div>
-            );
-          })}
+        <label> List of users: </label>
+          <ul id="listUsers" >
+          </ul>
         </div>
       </>
     );
