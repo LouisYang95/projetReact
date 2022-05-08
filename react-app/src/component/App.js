b42e6aa8c115f8f6bb2ddc726b23a429bd4ac369
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-05-08 02:29:33 +0200

    Adding some css
    Added some css to make it clearer to read and look

--- react-app/src/component/App.js
+++ react-app/src/component/App.js
@@ -4,39 +4,82 @@ import Users from "./Users";
 import UsersData from "../data.json";
 
 class App extends React.Component {
-  state = {};
+
+// function that will be called when the page is loaded
 
   componentDidMount = () => {
     const datalist = document.getElementById(`datalist`);
     datalist.innerHTML = "";
-    console.log("ok");
+
     UsersData.map(async (info) => {
+      var li = document.createElement("li");
+      var listUsers = document.getElementById(`listUsers`);
+      listUsers.appendChild(li);
+      li.innerHTML = info.name ;
       for (let departments of info.departments) {
         const response = await fetch(
-          `https://geo.api.gouv.fr/departements/${departments}?fields=nom`
+          `https://geo.api.gouv.fr/departements/${departments}?fields=*`
         );
         const data = await response.json();
         var optionBlock = document.createElement("option");
-        datalist.appendChild(optionBlock);
-        optionBlock.innerHTML = data.nom;
-        // if data.nom already contains in datalist, do nothing
         if (datalist.innerHTML.includes(data.nom)) {
           console.log("already contains");
           return;
         } else {
-          console.log("not contains");
+          datalist.appendChild(optionBlock);
+          optionBlock.innerHTML = data.nom;
           optionBlock.setAttribute("key", data.code);
-          optionBlock.setAttribute("value", data.nom);
         }
       }
     });
   };
 
+//Function that will display list of users thanks to geo.api.gouv
+
+  listUsers = () => {
+    const datalist = document.getElementById(`datalist`);
+    var optionValue = datalist.value;
+    var listUsers = document.getElementById(`listUsers`);
+    listUsers.innerHTML = "";
+    fetch(
+      `https://geo.api.gouv.fr/departements?nom=${optionValue}&fields=*`
+    ).then((response) =>
+      response.json().then((data) => {
+        for (let dataUsers of data) {
+          var dataDepartments = parseInt(dataUsers.code);
+
+          for (let users of UsersData) {
+            for (let departments of users.departments) {
+              var li = document.createElement("li");
+              if (departments === dataDepartments) {
+                listUsers.appendChild(li);
+                li.innerHTML = users.name;
+              }
+            }
+          }
+        }
+      })
+    );
+  };
+
+//display list of users while user click on btn
+  displayUsers = () => {
+    var listUsers = document.getElementById(`listUsers`);
+    listUsers.innerHTML = "";
+    UsersData.map(async (info) => {
+      var li = document.createElement("li");
+      listUsers.appendChild(li);
+      // li.innerHTML = info.name + " " + info.departments;
+      li.innerHTML = info.name;
+    });
+  };
+
+
   render() {
     return (
       <>
         <div>
-          <Search renderDepartments={this.renderDepartments} />
+          <Search listUsers={this.listUsers} displayUsers={this.displayUsers} />
         </div>
         <div>
           <Users />
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./component/App";
-import "./css/users.css";
+import "./css/index.css";
 
-
+//react 18.6.0
 const container = document.getElementById('root');
 const root = createRoot(container);
-// ReactDOM.render(<App />, document.getElementById("root"));
 root.render(<App tab="home" />);
+
+// ReactDOM.render(<App />, document.getElementById("root")); // old version of react
