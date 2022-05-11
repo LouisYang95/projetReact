5db6901a93563812611f92f201a092eae5b0f421
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-05-11 15:23:01 +0200

    Update App.js
    Replacing var

--- react-app/src/component/App.js
+++ react-app/src/component/App.js
@@ -5,7 +5,7 @@ import UsersData from "../data.json";
 
 class App extends React.Component {
 
-// function that will be called when the page is loaded
+  // function that will be called when the page is loaded
 
   componentDidMount = () => {
     const datalist = document.getElementById(`datalist`);
@@ -15,13 +15,13 @@ class App extends React.Component {
       var li = document.createElement("li");
       var listUsers = document.getElementById(`listUsers`);
       listUsers.appendChild(li);
-      li.innerHTML = info.name ;
+      li.innerHTML = info.name;
       for (let departments of info.departments) {
         const response = await fetch(
           `https://geo.api.gouv.fr/departements/${departments}?fields=*`
         );
         const data = await response.json();
-        var optionBlock = document.createElement("option");
+        let optionBlock = document.createElement("option");
         if (datalist.innerHTML.includes(data.nom)) {
           console.log("already contains");
           return;
@@ -34,23 +34,23 @@ class App extends React.Component {
     });
   };
 
-//Function that will display list of users thanks to geo.api.gouv
+  //Function that will display list of users thanks to geo.api.gouv
 
   listUsers = () => {
     const datalist = document.getElementById(`datalist`);
-    var optionValue = datalist.value;
-    var listUsers = document.getElementById(`listUsers`);
+    let optionValue = datalist.value;
+    const listUsers = document.getElementById(`listUsers`);
     listUsers.innerHTML = "";
     fetch(
       `https://geo.api.gouv.fr/departements?nom=${optionValue}&fields=*`
     ).then((response) =>
       response.json().then((data) => {
         for (let dataUsers of data) {
-          var dataDepartments = parseInt(dataUsers.code);
+          let dataDepartments = parseInt(dataUsers.code);
 
           for (let users of UsersData) {
             for (let departments of users.departments) {
-              var li = document.createElement("li");
+              let li = document.createElement("li");
               if (departments === dataDepartments) {
                 listUsers.appendChild(li);
                 li.innerHTML = users.name;
@@ -62,12 +62,12 @@ class App extends React.Component {
     );
   };
 
-//display list of users while user click on btn
+  //display list of users while user click on btn
   displayUsers = () => {
-    var listUsers = document.getElementById(`listUsers`);
+    const listUsers = document.getElementById(`listUsers`);
     listUsers.innerHTML = "";
     UsersData.map(async (info) => {
-      var li = document.createElement("li");
+      let li = document.createElement("li");
       listUsers.appendChild(li);
       // li.innerHTML = info.name + " " + info.departments;
       li.innerHTML = info.name;
